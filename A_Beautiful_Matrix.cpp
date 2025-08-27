cdd5c72784763c958c121b96d127be48f72c127a
Lakshya Patel <lakshyapatel.lp2005@gmail.com>
2025-08-28 04:15:59 +0530

    update

--- A_Beautiful_Matrix.cpp
+++ A_Beautiful_Matrix.cpp
@@ -16,3 +16,51 @@ Print a single integer — the minimum number of moves needed to make the matrix
 
 */
 
+# include <iostream>
+using namespace std;
+
+int main(){
+    int mat[5][5];
+
+    for(int i=0 ; i<5 ; i++){
+        for(int j=0 ; j<5 ; j++){
+            cin >> mat[i][j];
+        }
+    }
+
+    int a, b;
+
+    for(int i=0 ; i<5 ; i++){
+        for(int j=0 ; j<5 ; j++){
+            if(mat[i][j]==1){
+                a = i;
+                b = j;
+            }
+        }
+    }
+
+    int count = 0;
+
+    while(a!=2 && b!=2){
+        if(a>2){
+            a--;
+            count++;
+        }
+        if(a<2){
+            a++;
+            count++;
+        }
+        if(b>2){
+            b--;
+            count++;
+        }
+        if(b<2){
+            b++;
+            count++;
+        }
+    }
+
+    cout << count;
+
+    return 0;
+}
